d9b63a5dd47c43759a913d826013024053216c37
Nopass0 <gal.bogdan2015@gmail.com>
2024-01-23 22:48:56 +0500

    tree

--- frontend/src/components/TreeSelectCustom/index.tsx
+++ frontend/src/components/TreeSelectCustom/index.tsx
@@ -1,87 +1,111 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react'
+import s from './index.module.scss'
 
-// 1. Extend the Option Type to include suboptions
-type Option = {
-    value: string;
-    label: string;
-    subOptions?: Option[];
-};
+export type Option = {
+	value: string
+	label: string
+	subOptions?: Option[]
+	expand?: boolean // Optional, if you want to control the initial expand state from the options data
+}
 
-type TreeSelectCustomProps = {
-    options: Option[];
-    onChange: (selectedValues: string[]) => void;
-};
+export type TreeSelectCustomProps = {
+	options: Option[]
+	onChange: (selectedValues: string[]) => void
+}
 
-const TreeSelectCustom: React.FC<TreeSelectCustomProps> = ({ options, onChange }) => {
-    // State to keep track of selected options
-    const [selectedValues, setSelectedValues] = useState<string[]>(['', '', '']);
+export const TreeSelectCustom: React.FC<TreeSelectCustomProps> = ({
+	options,
+	onChange,
+}) => {
+	const [selectedValues, setSelectedValues] = useState<string[]>(['', '', ''])
+	const [checkedValues, setCheckedValues] = useState<{[key: string]: boolean}>(
+		{},
+	)
+	const [expandedValues, setExpandedValues] = useState<{
+		[key: string]: boolean
+	}>({})
 
-    // State to keep track of checked options (for checkboxes)
-    const [checkedValues, setCheckedValues] = useState<{ [key: string]: boolean }>({});
+	const handleSelectChange = (value: string, index: number) => {
+		const updatedValues = [...selectedValues]
+		updatedValues[index] = value
+		setSelectedValues(updatedValues)
+		onChange(updatedValues)
+	}
 
-    const handleSelectChange = (value: string, index: number) => {
-        const updatedValues = [...selectedValues];
-        updatedValues[index] = value;
-        setSelectedValues(updatedValues);
-        onChange(updatedValues);
-    };
+	const handleCheckboxChange = (option: Option, isChecked: boolean) => {
+		const newCheckedValues = {...checkedValues}
 
-    const handleCheckboxChange = (option: Option, isChecked: boolean) => {
-        // Update the checked state
-        setCheckedValues({
-            ...checkedValues,
-            [option.value]: isChecked,
-        });
+		const setCheckedRecursively = (opt: Option, checked: boolean) => {
+			newCheckedValues[opt.value] = checked
 
-        // Propagate changes to suboptions if any
-        if (option.subOptions) {
-            option.subOptions.forEach((subOption) => {
-                handleCheckboxChange(subOption, isChecked);
-            });
-        }
-    };
+			if (opt.subOptions) {
+				opt.subOptions.forEach((subOption) => {
+					setCheckedRecursively(subOption, checked)
+				})
+			}
+		}
 
-    const renderOptions = (options: Option[], depth: number = 0) => (
-        <ul style={{ paddingLeft: depth * 20 }}>
-            {options.map((option) => (
-                <li key={option.value}>
-                    <label>
-                        <input
-                            type="checkbox"
-                            checked={checkedValues[option.value] || false}
-                            onChange={(e) => handleCheckboxChange(option, e.target.checked)}
-                            // Indeterminate state for half-checked
-                            ref={(el) => {
-                                if (el) el.indeterminate = checkedValues[option.value] === undefined;
-                            }}
-                        />
-                        {option.label}
-                    </label>
-                    {option.subOptions && renderOptions(option.subOptions, depth + 1)}
-                </li>
-            ))}
-        </ul>
-    );
+		setCheckedRecursively(option, isChecked)
 
-    return (
-        <div>
-            {Array.from({ length: 3 }).map((_, index) => (
-                <select
-                    key={index}
-                    value={selectedValues[index]}
-                    onChange={(e) => handleSelectChange(e.target.value, index)}
-                >
-                    <option value="">Select an option</option>
-                    {options.map((option) => (
-                        <option key={option.value} value={option.value}>
-                            {option.label}
-                        </option>
-                    ))}
-                </select>
-            ))}
-            {renderOptions(options)}
-        </div>
-    );
-};
+		setCheckedValues(newCheckedValues)
+	}
 
-export default TreeSelectCustom;
+	const toggleExpand = (option: Option) => {
+		setExpandedValues({
+			...expandedValues,
+			[option.value]: !expandedValues[option.value],
+		})
+	}
+
+	const handleRemoveChip = (value: string) => {
+		handleCheckboxChange({value, label: '', subOptions: []}, false)
+	}
+
+	const renderOptions = (options: Option[], depth: number = 0) => (
+		<ul style={{paddingLeft: depth * 20}}>
+			{depth > 0 && <hr />}
+			{options.map((option) => (
+				<li key={option.value}>
+					<div>
+						{option.subOptions && (
+							<button onClick={() => toggleExpand(option)}>
+								{expandedValues[option.value] ? '-' : '+'}
+							</button>
+						)}
+						<label>
+							<input
+								type="checkbox"
+								checked={checkedValues[option.value] || false}
+								onChange={(e) => handleCheckboxChange(option, e.target.checked)}
+								ref={(el) => {
+									if (el)
+										el.indeterminate = checkedValues[option.value] === undefined
+								}}
+							/>
+							{option.label}
+						</label>
+					</div>
+					{option.subOptions &&
+						expandedValues[option.value] &&
+						renderOptions(option.subOptions, depth + 1)}
+				</li>
+			))}
+		</ul>
+	)
+
+	return (
+		<div>
+			<div className={s.chipContainer}>
+				{Object.entries(checkedValues)
+					.filter(([_, checked]) => checked)
+					.map(([value, _]) => (
+						<div key={value} className={s.chip}>
+							<span>{value}</span>
+							<button onClick={() => handleRemoveChip(value)}>x</button>
+						</div>
+					))}
+			</div>
+			{renderOptions(options)}
+		</div>
+	)
+}
